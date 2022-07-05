1a73f917862ab3482aa8a85bf4ba1286146225d1
Iaroslav Os <osmnbx@gmail.com>
2022-07-05 10:06:38 +0300

    upd: test conn->db

---
+++ sources/main.py
@@ -0,0 +1,28 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'key'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@db/smarket_test'
+
+
+db = SQLAlchemy(app)
+
+
+class DataTest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    value = db.Column(db.String(200))
+
+    def __repr__(self):
+        return f'my_table: {self.id}, {self.value}'
+
+
+@app.route('/')
+def index():
+    return 'smarket home page'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port='5000')
