f41308d2a50023cd9ff29c4f765e4c0d4e53aab6
Iaroslav Os <osmnbx@gmail.com>
2022-07-06 10:59:34 +0300

    updated main: added db class item (db->model)

--- sources/market.py
+++ sources/market.py
@@ -1,24 +1,27 @@
-from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 # Create an app
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'key'
 # Off console warnings
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# MySQL connection: server 'db', database name 'smarket_test'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@db/smarket_test'
+# MySQL connection: server 'db', database name 'smarket_data'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@db/smarket_data'
 
 
 db = SQLAlchemy(app)
 
 
-class DataTest(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    value = db.Column(db.String(200))
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(12), nullable=False)
+    description = db.Column(db.String(500), nullable=False, unique=True)
 
     def __repr__(self):
-        return f'my_table: {self.id}, {self.value}'
+        return f'Item {self.name}'
 
 
 @app.route('/')
@@ -29,13 +32,11 @@ def home_page():
 
 @app.route('/market')
 def market_page():
-    items = [
-        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-    ]
+    items = Item.query.all()
     return render_template('market.html', items=items)
 
+
+db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='5000')
