aafd4568342fa3cd47ef42b6c2efa712bf5fcde9
Iaroslav Os <osmnbx@gmail.com>
2022-07-07 11:25:49 +0300

    added User model

--- sources/market/models.py
+++ sources/market/models.py
@@ -7,6 +7,17 @@ class Item(db.Model):
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(12), nullable=False)
     description = db.Column(db.String(500), nullable=False, unique=True)
-
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
     def __repr__(self):
         return f'Item {self.name}'
+
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(30), nullable=False, unique=True)
+    email_address = db.Column(db.String(50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(60), nullable=False)
+    budget = db.Column(db.Integer(), nullable=False, default=1000)
+    items = db.relationship('Item', backref='owned_user', lazy=True)
+    
+    
