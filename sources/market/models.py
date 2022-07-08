5fa82dab3660f1ec3aca86ecd384a5dae3b0d82b
Iaroslav Os <osmnbx@gmail.com>
2022-07-08 13:05:43 +0300

    added __repr__ method

--- sources/market/models.py
+++ sources/market/models.py
@@ -3,21 +3,27 @@ from market import db
 
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
+    
     name = db.Column(db.String(50), nullable=False, unique=True)
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(12), nullable=False)
     description = db.Column(db.String(500), nullable=False, unique=True)
     owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    
     def __repr__(self):
         return f'Item {self.name}'
 
 
 class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
+    
     username = db.Column(db.String(30), nullable=False, unique=True)
     email_address = db.Column(db.String(50), nullable=False, unique=True)
     password_hash = db.Column(db.String(60), nullable=False)
     budget = db.Column(db.Integer(), nullable=False, default=1000)
     items = db.relationship('Item', backref='owned_user', lazy=True)
+    
+    def __repr__(self):
+        return f'User {self.username} ({self.id})'
     
     
