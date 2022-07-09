6359b3bf9e3f0ea11c54e093808123d51c4679aa
Iaroslav Os <osmnbx@gmail.com>
2022-07-09 12:19:10 +0300

    imported bcrypt module

--- sources/market/__init__.py
+++ sources/market/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 # Create an app
 app = Flask(__name__)
@@ -10,5 +11,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@db/smarket_data'
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from market import routes
