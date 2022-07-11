9ebd0ce7d9493733acab9ed1c9f0c744a581f00d
Iaroslav Os <osmnbx@gmail.com>
2022-07-11 17:01:22 +0300

    login after register

--- sources/market/routes.py
+++ sources/market/routes.py
@@ -28,6 +28,8 @@ def register_page():
                               password=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
+        login_user(user_to_create)
+        flash(f'Account created successfully! You are now logged in as {user_to_create}', category='success')
         return redirect(url_for('market_page'))
     for err_msg in form.errors.values():
         flash(f'There was an error with creating a user: {err_msg}', category='danger')
