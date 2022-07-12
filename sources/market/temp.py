d69a125644da1f6d401fbc5257b56b7b23f16305
Iaroslav Os <osmnbx@gmail.com>
2022-07-12 13:52:16 +0300

    final upd temp.py

--- sources/market/temp.py
+++ sources/market/temp.py
@@ -1,7 +1,14 @@
 from market import db
 from market.models import Item, User
+from random import randint as ran
 
 users = User.query.all()
 items = Item.query.all()
 
 # from market.temp import *
+
+def go():
+    for i in range(11,20):
+        i = Item(name=f"New Item #{i}", price=ran(5,1000), barcode=f"{ran(111111111111,999999999999)}", description=f"description # {i}")
+        db.session.add(i)
+        db.session.commit()
